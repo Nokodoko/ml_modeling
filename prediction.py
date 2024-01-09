7f7a97846ae9de9f03e1756278e12b004f6f4f4f
Nokodoko <cMonty614@gmail.com>
2024-01-09 00:18:41 -0500

    Over/Underfitting
    Added max leaf nodes to controll for over/under fitting, via function. Then with 4 different values for max leafs, look at updated mae values

--- prediction.py
+++ prediction.py
@@ -5,26 +5,30 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 # CONSTANTS
-file = './melb_data.csv'
-data = pd.read_csv(file)
-base = data.dropna(axis=0)
+FILE = './melb_data.csv'
+DATA = pd.read_csv(FILE)
+BASE = DATA.dropna(axis=0)
 
 
-def y(attr):  # PREDICTION
-    y = base[attr]
-    return y
+def Y(attr):
+    return BASE[attr]
 
 
 # UPDATE PREDICTION HERE
 prediction_feature = 'Price'
 
-features = ['Landsize', 'Car',
-            'Bedroom2', 'Postcode', 'Propertycount']
+features = [
+    'Landsize',
+    'Car',
+    'Bedroom2',
+    'Postcode',
+    'Propertycount'
+]
 
-X = base[features]
+X = BASE[features]
 
 tr_X, val_X, tr_y, val_y = train_test_split(
-    X, y(prediction_feature), random_state=0)
+    X, Y(prediction_feature), random_state=0)
 
 val_base_model = DecisionTreeRegressor()
 val_base_model.fit(tr_X, tr_y)
@@ -33,11 +37,24 @@ val_mae = mean_absolute_error(val_y, val_predictions)
 print(f"Validation Predictions MAE: {val_mae}")
 
 base_model = DecisionTreeRegressor(random_state=0)
-base_model.fit(X, y(prediction_feature))
+base_model.fit(X, Y(prediction_feature))
 predictions = base_model.predict(X.head())
 print(f"Top 5 predictions:{predictions}")
 
 
 predicted_prices = base_model.predict(X)
-mae = mean_absolute_error(y(prediction_feature), predicted_prices)
-print(f"MAE/in sample score: {mae}")
+
+
+def get_mae(max_leaf_nodes, train_X, value_X, train_Y, value_Y):
+    model = DecisionTreeRegressor(
+        max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_Y)
+    predictions = model.predict(value_X)
+    mae = mean_absolute_error(value_Y, predictions)
+    return mae
+
+
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    controlled_mae = get_mae(max_leaf_nodes, tr_X, val_X, tr_y, val_y)
+    print(
+        f"Max Leaf Nodes: {max_leaf_nodes} \t\t Mean Absolute Error: {controlled_mae}")
